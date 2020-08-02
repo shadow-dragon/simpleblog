e0d0169eb5aee0a34ab2830fc8cf1eef1bf58183
Mayur <mayurmallya7@gmail.com>
2020-08-02 14:16:36 +0530

    Created private method post_params and changes to create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,5 +9,14 @@ class PostsController < ApplicationController
 
     def create
         #render plain: params[:post]
+        @post = Post.new(post_params)
+        @post.save
+	    redirect _to @post
+    end 
+
+
+    private def post_params
+        params.require(:post).permit(:title, :body)
     end
+
 end
