18ba0a56786faea7b4b567586ed3122d95b3e6dc
Mayur <mayurmallya7@gmail.com>
2020-08-06 12:52:30 +0530

    Moved creation and form of comments to respective partials

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
